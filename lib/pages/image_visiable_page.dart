880c388e1959b57a357f9223ff0b8be2e0d7dcc2
Hakimbek <hakimbekmrx@gmail.com>
2023-04-28 17:39:22 +0500

    003

---
+++ lib/pages/image_visiable_page.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ImagePage extends StatefulWidget {
+  final String? url;
+  final File? file;
+  const ImagePage({Key? key, this.url, this.file}) : super(key: key);
+
+  @override
+  State<ImagePage> createState() => _ImagePageState();
+}
+
+class _ImagePageState extends State<ImagePage> {
+  bool fullScreen = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        iconTheme: IconThemeData(
+          color: Colors.white
+        ),
+        title: Text("Rasmni ko'rish",style: TextStyle(color: Colors.white)),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                fullScreen = !fullScreen;
+              });
+            },
+            icon: fullScreen?Icon(CupertinoIcons.fullscreen_exit):Icon(CupertinoIcons.fullscreen,),
+          )
+        ],
+      ),
+      body: Center(
+        child: widget.file == null?
+        fullScreen?
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: CachedNetworkImage(
+            fit: BoxFit.cover,
+            progressIndicatorBuilder: (context, url, progress) => Center(child: CircularProgressIndicator(color: Colors.white,),),
+            imageUrl: widget.url!,
+          ),
+        ):
+        CachedNetworkImage(
+          progressIndicatorBuilder: (context, url, progress) => Center(child: CircularProgressIndicator(color: Colors.white,),),
+          imageUrl: widget.url!,
+        ):
+        fullScreen?
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Image(
+            fit: BoxFit.cover,
+            image: FileImage(widget.file!),
+          ),
+        ):
+        Image.file(widget.file!),
+      )
+    );
+  }
+}
